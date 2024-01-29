6690e24d962a0cc7735ebbd9d7270d290c2a462b
hr-jin <584289035jhr@gmail.com>
2024-01-29 22:31:13 +0800

    fix a bug in convex_optim

--- extractors/convex_optim.py
+++ extractors/convex_optim.py
@@ -224,7 +224,7 @@ def ISTA_PN(I,basis,lambd,num_iter,eta=None, useMAGMA=True):
     M = basis.size(1)
     if eta is None:
         if useMAGMA:
-            L = torch.max(torch.symeig(torch.mm(basis,basis.t()),eigenvectors=False)[0])
+            L = torch.max(torch.linalg.eigvalsh(torch.mm(basis,basis.t())))
             eta = 1./L
         else:
             eta = 1./cp.linalg.eigvalsh(cp.asarray(torch.mm(basis,basis.t()).cpu().numpy())).max().get().reshape(1)
@@ -251,7 +251,7 @@ def FISTA(I,basis,lambd,num_iter, device, eta=None, useMAGMA=True):
     M = basis.size(1)
     if eta is None:
         if useMAGMA:
-            L = torch.max(torch.symeig(torch.mm(basis,basis.t()),eigenvectors=False)[0])
+            L = torch.max(torch.linalg.eigvalsh(torch.mm(basis,basis.t())))
             eta = 1./L
         else:
             eta = 1./cp.linalg.eigvalsh(cp.asarray(torch.mm(basis,basis.t()).cpu().numpy())).max().get().reshape(1)
@@ -282,7 +282,7 @@ def ISTA(I,basis,lambd,num_iter,eta=None, useMAGMA=True):
     M = basis.size(1)
     if eta is None:
         if useMAGMA:
-            L = torch.max(torch.symeig(torch.mm(basis,basis.t()),eigenvectors=False)[0])
+            L = torch.max(torch.linalg.eigvalsh(torch.mm(basis,basis.t())))
             eta = 1./L
         else:
             eta = 1./cp.linalg.eigvalsh(cp.asarray(torch.mm(basis,basis.t()).cpu().numpy())).max().get().reshape(1)
